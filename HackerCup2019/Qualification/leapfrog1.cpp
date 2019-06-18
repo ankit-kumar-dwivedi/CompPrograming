8be618202f4970fe2cea4fd4b3ca01fed78f9f97
Ankit Kumar Dwivedi <dwivedi.ankit21@gmail.com>
2019-06-18 22:05:31 +0530

    Added Hackercup qualication round

---
+++ HackerCup2019/Qualification/leapfrog1.cpp
@@ -0,0 +1,36 @@
+/*
+ Author     : Ankit Kumar Dwivedi
+ Institution: ASET Delhi
+*/
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+main()
+{
+    // #ifndef ONLINE_JUDGE
+    //     freopen("leapfrog_ch_.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    //
+    int t,n;
+    string str;
+    cin>>t;
+    for (int i = 0; i < t; ++i)
+    {
+        cin>>str;
+        int dot = count(str.begin(),str.end(),'.');
+        int b = count(str.begin(),str.end(),'B');
+        if((dot)&&(b)&&(dot <= b))
+        cout<<"Case #"<<i+1<<": Y\n";
+        else
+        {
+                    cout<<"Case #"<<i+1<<": N\n";
+        }
+            
+    }
+    return 0;
+
+}
